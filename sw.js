067a0327f116b377dad97135c76036964808ba2f
sleepfox76 <sleepfox76@gmail.com>
2026-04-17 22:19:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE = 'stock-tracker-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const ASSETS = [
+  '/stock-tracker/',
+  '/stock-tracker/index.html',
+  '/stock-tracker/manifest.json',
+  '/stock-tracker/icon-192.png',
+  '/stock-tracker/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
@@ -7,12 +13,16 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).catch(() => caches.match('/stock-tracker/index.html'))
+    )
   );
 });
