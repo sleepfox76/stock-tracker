aa2b8a3d55c5b82bde27d26ef01a20c1f69def71
sleepfox76 <sleepfox76@gmail.com>
2026-04-25 23:31:31 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'stock-tracker-v6';
+const ASSETS = [
+    '/stock-tracker/',
+    '/stock-tracker/index.html',
+    '/stock-tracker/manifest.json',
+    '/stock-tracker/icon-192.png',
+    '/stock-tracker/icon-512.png'
+  ];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys =>
+          Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+                                     ));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+          caches.match(e.request).then(cached =>
+                  cached || fetch(e.request).catch(() => caches.match('/stock-tracker/index.html'))
+                                           )
+        );
+});
